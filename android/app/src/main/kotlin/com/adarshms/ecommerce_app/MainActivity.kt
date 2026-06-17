70491c5724d39f1d4439a915b047dc7518701b99
adarshms32 <adharshadharshms4036@gmail.com>
2026-06-17 11:28:07 +0530

    Day 1: Flutter ecommerce project setup

---
+++ android/app/src/main/kotlin/com/adarshms/ecommerce_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.adarshms.ecommerce_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/utils/theme/custom_themes/bottom_sheet_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TBottomSheetTheme {
+  TBottomSheetTheme._();
+
+  static BottomSheetThemeData lightBottomSheetTheme =
+  const BottomSheetThemeData(
+    showDragHandle: true,
+    backgroundColor: Colors.white,
+    modalBackgroundColor: Colors.white,
+    constraints: BoxConstraints(minWidth: double.infinity),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(
+        top: Radius.circular(16),
+      ),
+    ),
+  );
+
+  static BottomSheetThemeData darkBottomSheetTheme =
+  const BottomSheetThemeData(
+    showDragHandle: true,
+    backgroundColor: Colors.black,
+    modalBackgroundColor: Colors.black,
+    constraints: BoxConstraints(minWidth: double.infinity),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(
+        top: Radius.circular(16),
+      ),
+    ),
+  );
+}
